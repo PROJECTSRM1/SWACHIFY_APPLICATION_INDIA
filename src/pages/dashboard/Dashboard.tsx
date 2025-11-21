ee8868e9b72fbf63970299404d2ba5ae8c2204c1
HarishyadavRM1 <harishyadav.pilli@rm1codershub.com>
2025-11-21 15:54:11 +0530

    n

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -4,9 +4,7 @@ import "./Dashboard.css";
 import ConstructionServices from "../building/building";
 import Packersandmovers from "./PackersAndMovers/Packersandmovers";
 import CleaningService from "./cleaningservice/CleaningService";
-<<<<<<< HEAD
 import BuySaleProducts from "./buy&sale/BuySaleProducts";
-=======
 import HomeServices from "./homeservices/HomeServices";
 import { Route, Routes } from "react-router-dom";
 import ServicesPage from "./homerentals/pages/ServicesPage";
@@ -18,7 +16,6 @@ import CommercialListingsPage from "./homerentals/pages/CommercialListingsPage";
 import FooterBar from "./homerentals/components/FooterBar";
 import HeaderBarforrental from "./homerentals/components/HeaderBar";
 
->>>>>>> main
 const Dashboard: React.FC = () => {
   return (
     <div className="dashboard-container">
