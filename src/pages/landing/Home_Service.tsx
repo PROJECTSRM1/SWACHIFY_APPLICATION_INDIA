eaa829fd48dfffc0be13ee70c79bc3d627100417
HarishyadavRM1 <harishyadav.pilli@rm1codershub.com>
2025-11-27 18:38:27 +0530

    changes

--- src/pages/landing/Home_Service.tsx
+++ src/pages/landing/Home_Service.tsx
@@ -47,20 +47,6 @@ const { Title, Paragraph } = Typography;
 const { Option } = Select;
 
 
-<<<<<<< HEAD
-const navItems = [
-  { key: "home", label: <Link to="/landing">Home</Link> },
-  { key: "cleaning", label: <Link to="/cleaningservice">Cleaning</Link> },
-  { key: "packers", label: <Link to="/LandingPackers">Packers & Movers</Link> },
-  { key: "home_services", label: <Link to="/home_service">Home Services</Link> },
-  { key: "rentals", label: <Link to="/rentals">Rentals</Link> },
-  { key: "commercial", label: <Link to="/commercial-plots">Buy&Sale Properties</Link> },
-  { key: "materials", label: <Link to="/ConstructionMaterials">Construction Materials</Link> },
-  { key: "freelancer", label: <Link to="/Freelancer">Freelancer</Link> },
-    // { key: "location", label: <Link to="">Location</Link> },
-];
-=======
->>>>>>> main
 
 const cardRows = [
   [
