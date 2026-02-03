637afc448712f413804b1978ace8179a2d2a37c9
yeswanthreddyRM1 <Tonangi.Yeswanth@rm1codershub.com>
2026-02-03 15:58:39 +0530

    changed educationpage

--- src/pages/landing/EducationPage.tsx
+++ src/pages/landing/EducationPage.tsx
@@ -118,7 +118,7 @@ const EducationPage = () => {
                 <Users size={20} />
               </div>
               <Counter end={15000} start={startCount} />
-              <span className="stat-label">Students Enrolled</span>
+              <span>Students Enrolled</span>
             </div>
 
             <div className="sw-ep-stat-item">
